d71121a6e1662880dcdedb0b5f7a61656fa18f44
Jagruti Tiwari <tiwarijagruti1@gmail.com>
2017-07-29 23:46:26 +0530

    committed 2 files inserted into user_sub_map,user_class

---
+++ sql/scripts/insert_user_sub_map.sql
@@ -0,0 +1,3 @@
+insert into user_sub_map(user_id,class_id)values('aps6735@yahoo.com',1);
+insert into user_sub_map(user_id,class_id)values('prajwalashetty96@gmail.com',2);
+insert into user_sub_map(user_id,class_id)values('Suhail Barot',3);
