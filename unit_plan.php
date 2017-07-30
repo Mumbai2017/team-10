5d0120a30c81a070aafdbd8d5289e97e7221afa6
HarshitVirkar <hvirkar36@gmail.com>
2017-07-30 10:58:45 +0530

    final unit_plan.php

--- unit_plan.php
+++ unit_plan.php
@@ -1,10 +1,50 @@
 <?php
-	session_start();
-	if(!isset($_SESSION['role']))
-	{
-		header("location: login.php");
-	}
+
+//echo "Here";
+session_start();
+if(!isset($_SESSION['role']))
+{
+	
+    header('location:login.php');
+}
+//echo "skipped";
+if(isset($_POST)) {
+	//echo "In post";
+include("config.php");
+$un = $_POST['unit_name'];
+$theme_unit = $_POST['theme_unit'];
+$lessons=$_POST['lesson'];
+//$subj=$_POST["Subject"];
+/*
+$sql = "select subj_id from subjects where subject=$subj ;
+
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        $subj_id=row['subj_id'];
+    }
+
+*/
+$sql = "INSERT INTO units (unit_name,theme_unit,no_of_lesson) VALUES ('$un','$theme_unit','$lessons')";
+
+if(mysqli_query($db, $sql)){
+    //echo "Records inserted successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($db);
+}
+ 
+// Close connection
+mysqli_close($db);
+}
 ?>
+
+
+
+
+
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -18,6 +58,7 @@
 				<span>&#9776</span>
 			</div>
 		</div>
+
 		<div class="jumbotron">
 			<div class="container">
 				<div class="section_header">
@@ -25,7 +66,7 @@
 				</div>
 				<hr>
 				<div class="form_container">
-					<form>
+					<form method="POST" action="">
 						<div class="form-group">
 					    	<label for="unit_name">Unit Name</label>
 					    	<input type="text" class="form-control" id="unit_name" name="unit_name">
@@ -35,16 +76,16 @@
 					    	<input type="text" class="form-control" id="theme_unit" name="theme_unit">
 					  	</div>
 					  	<div class="form-group">
-					    	<label for="lesson_1">Lesson no</label>
-					    	<input type="text" class="form-control" id="lesson-no-1" name="lesson_1">
+					    	<label for="lesson_1">Number of Lessons </label>
+					    	<input type="text" class="form-control" id="lesson" name="lesson">
 					  	</div>
 					  	<div class="form-group">
-					    	<label for="lesson_2">Lesson no</label>
-					    	<input type="text" class="form-control" id="lesson-no-2" name="lesson_2">
+					    	<label for="Subject">Subject Name</label>
+					    	<input type="text" class="form-control" id="Subject" name="Subject">
 					  	</div>
 					  	<div class="form-group">
-					    	<label for="lesson_3">Lesson no</label>
-					    	<input type="text" class="form-control" id="lesson-no-3" name="lesson_3">
+					    	<label for="standard">Standard</label>
+					    	<input type="text" class="form-control" id="standard" name="standard">
 					  	</div>
 					  	<div style="text-align: center; margin-top: 30px">
 					  		<button type="submit" class="btn btn-default btn-success" style="width: 120px">Submit</button>
@@ -58,13 +99,19 @@
 				<span style="color: black">X</span>
 			</div>
 			<div class="side_bar_opt">
+				<a href="/profile.php">Teacher Profile</a>
+			</div>
+			<div class="side_bar_opt">
+				<a href="/quick_start.php">Upload Video</a>
+			</div>
+			<div class="side_bar_opt">
 				<a href="/unit_plan.php">Submit User Plan</a>
 			</div>
 			<div class="side_bar_opt">
 				<a href="/lesson_plan.php">Submit Lesson Plan</a>
 			</div>
 			<div class="side_bar_opt">
-				<a href="/update_learning.php">Update Learning Plan</a>
+				<a href="/get_feedback.php">Get feedbacks</a>
 			</div>
 			<div class="side_bar_opt">
 				<a href="/logout.php">Logout</a>
