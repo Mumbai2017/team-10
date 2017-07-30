a15c9a65db3f79715a715ee5ae35209c52b77fd9
Jagruti Tiwari <tiwarijagruti1@gmail.com>
2017-07-30 10:44:54 +0530

    Final commit

---
+++ sme.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<style>
+
+img  {border: black;
+	size="3px";
+}
+p    {color: red;}
+</style>
+</head>
+<body>
+
+<center><h2>Our Teachers</h2></center>
+
+
+<div class="container">
+<center>
+<table cellspacing="50px" >
+<tr >
+<td>
+  <a href="#"><img  style="border:5px solid black" color="black" src="img4.jpg"  class="image" width=200 height=200> </a> 
+  </td>
+  <td>
+  <a href="#"><img style="border:5px solid black" src="img1.jpg"  class="image" width=200 height=200> </a> 
+  </td>
+  <td>
+  <a href="#"><img style="border:5px solid black" src="img2.jpg"  class="image" width=200 height=200> </a> 
+  </td>
+  <td>
+  <a href="#"><img style="border:5px solid black" src="img3.jpg"  class="image" width=200 height=200> </a> 
+  </td>
+
+  </tr>
+
+  <tr>
+  <td align="center"> <font size=5><a href="#">Teacher 1</a> </font>
+  
+  </td>
+
+  <td align="center"><font size=5><a href="#">Teacher 2</a> </font>
+  
+  </td>
+  <td align="center"><font size=5><a href="#">Teacher 3</a></font> </td>
+  
+  <td align="center"><font size=5><a href="#">Teacher 4</a> </font>
+  
+  </td>
+  </tr>
+
+  </center>
+  </table>
+
+
+</div>
+</body>
+</html>
